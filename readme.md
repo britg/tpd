f891b36364b18032e9987449a7029f6c10b95f15
britg <brit@britg.com>
2015-09-02 06:39:09 -0500

    Merge other libraries into tpd

--- readme.md
+++ readme.md
@@ -1,3 +1,15 @@
 # Tavernpunk Unity3d Helpers
 
 A one file script where I put all my cross-project helper functions for Unity3d C# projects.
+
+Purpose:
+
+- one file
+- no dependencies
+- cross platform
+
+Maybe someday, far into the future, I will document each of them, but for now it's just a dump ¯\_(ツ)_/¯.
+
+### Usage
+
+Simply drop into a Unity3d assets folder.
--- tpd.cs
+++ tpd.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 /* Tavernpunk Unity3d Helpers
+ * Brit Gardner
+ * http://britg.com
+ * @britg
+ *
  * A set of helper classes that can be dropped into a
  * Unity3D assets folder.
 
@@ -17,6 +22,9 @@ THE SOFTWARE.
 public class tpd {
 
 
+  /*
+   * Strings
+   */
 
   public static bool BeginsWith (string s, string match) {
     if (s.Length < match.Length) {
@@ -24,6 +32,105 @@ public class tpd {
     }
 
     return (s.Substring(0, match.Length) == match);
+  }
+
+  public static string AddOrdinal (int num) {
+    if (num <= 0) return num.ToString();
+
+    switch (num % 100) {
+      case 11:
+      case 12:
+      case 13:
+        return num + "th";
+    }
+
+    switch (num % 10) {
+      case 1:
+        return num + "st";
+      case 2:
+        return num + "nd";
+      case 3:
+        return num + "rd";
+      default:
+        return num + "th";
+    }
+
+  }
+
+  public static string AddSign (int n) {
+    if (n > 0) return "+" + n;
+    return "-" + Mathf.Abs(n);
+  }
+
+  /*
+   * Randomization / Roll Percent
+   */
+
+  public static string RollSet (Hashtable hash) {
+    float sum = 0f;
+    foreach (DictionaryEntry pair in hash) {
+      sum += (float)pair.Value;
+    }
+
+    float rand = Random.Range(0f, sum);
+
+    float running = 0;
+    string chosen = null;
+    foreach (DictionaryEntry pair in hash) {
+      running += (float)pair.Value;
+      if (rand <= running) {
+        chosen = (string)pair.Key;
+        break;
+      }
+    }
+
+    return chosen;
+  }
+
+  public static string RollSet (Dictionary<string, float> dict) {
+    float sum = 0f;
+    foreach (KeyValuePair<string, float> pair in dict) {
+      sum += (float)pair.Value;
+    }
+
+    float rand = Random.Range(0f, sum);
+
+    float running = 0;
+    string chosen = null;
+    foreach (KeyValuePair<string, float> pair in dict) {
+      running += (float)pair.Value;
+      if (rand <= running) {
+        chosen = (string)pair.Key;
+        break;
+      }
+    }
+
+    return chosen;
+  }
+
+  public static bool RollPercent (float chance) {
+    float rand = Random.Range(0f, 100f);
+    return rand < chance;
+  }
+
+  public static float RollRange (RangeAttribute range) {
+    return Random.Range(range.min, range.max);
+  }
+
+  /*
+   * Colors
+   */
+
+  public static string ColorToHex (Color32 color) {
+    string hex = color.r.ToString("X2") + color.g.ToString("X2") + color.b.ToString("X2");
+    return hex;
+  }
+
+  public static Color HexToColor (string hex) {
+    byte r = byte.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+    byte g = byte.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
+    byte b = byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
+    return new Color32(r, g, b, 255);
   }
 
 }
